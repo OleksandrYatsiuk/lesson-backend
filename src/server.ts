e38def03612a7a0c859ecb18fd3aea0465a6d06b
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-12-22 08:43:25 +0200

    run server

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,9 @@
 import App from './app';
 import 'dotenv/config';
-import {
-	CertificateController,
-	CoursesController, LessonsController, MessagesController,
-	PagesController, PaymentsController, StudyProgressController, UsersController
-} from './controllers';
+
 
 
 const app = new App(
-	[
-		new PaymentsController(),
-		new UsersController(),
-		new CoursesController(),
-		new LessonsController(),
-		new MessagesController(),
-		new PagesController(),
-		new StudyProgressController(),
-		new CertificateController()
-	],
 	+process.env.PORT,
 	'/v1'
 );
